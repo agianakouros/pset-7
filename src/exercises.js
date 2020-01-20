6fd085b54acf56275d3383701a438db2a38503e0
Alex G <agianakouros@ucvts.org>
2020-01-20 16:10:25 -0500

    gotta fix the second one

--- src/exercises.js
+++ src/exercises.js
@@ -1,35 +1,83 @@
 function commonEnd(a, b) {
   if (!a || a.length === 0 || !b || b.length === 0) {
-    return false;
-  }
-
-var beginning_A = a[0]
-var end_A = a[a.length - 1]
-var beginning_B = b[0]
-var end_B = a[b.length - 1]
-
-    if (beginning_A === beginning_B || end_A === end_B || a !== undefined && b !== undefined && a < 1 && b < 1){
-    return true;
-  } else {
-    return false;
+  return false;
+}
+  if (a[0] === b[0] || a[a.length-1] === b[b.length-1]) {
+  return true;
+} else {
+  return false;
 }
 }
 
 function endsMeet(values, n) {
-  // write your code here
+  let a1 = [];
+  let a2 = [];
+  if (values.length < n || n < 1 || !values || !Number.isInteger(n)) {
+    return [];
+  }
+  else {
+      array1 = values.slice(0, n);
+      array2 = values.slice(values.length - n, values.length + 1);
+      newArray = array1.concat(array2);
+      return newArray;
+  }
 }
 
 function difference(numbers) {
-  // write your code here
+
+  if (!numbers || numbers.some(isNaN) || numbers.length < 1) {
+    return undefined;
+  }
+  else {
+      if (numbers.length === 1) {
+      return 0;
+    } else {
+      max = Number(Math.max.apply(null, numbers));
+      min = Number(Math.min.apply(null, numbers));
+    }
+    return max - min;
+  }
 }
 
 function max(number) {
-  // write your code here
+    if (!number || number.length < 3 || number.length % 2 == 0) {
+    return undefined;
+  } if (number.some(isNaN)) {
+    return undefined;
+  }
+
+  let last = number[number.length-1]
+  let middle = number[((number.length/2)-1) + 0.5]
+  let first = number[0]
+
+    if ((last > first) && (last > middle)) {
+    return last;
+  } else if ((first > middle) && (first > last)) {
+    return first;
+  } else if ((middle > last) && (middle > first)) {
+    return middle;
+  } else {
+    return first;
+  }
 }
 
 function middle(values) {
-  // write your code here
-}
+ let array = [];
+
+   if (values == undefined || values.length % 2 == 0 || values.length < 3 ){
+   return [];
+
+ } else {
+
+   let middle1 = values[values.length - (Math.ceil(values.length / 2))]
+   let middle2 = values[(values.length - 1) - (Math.ceil(values.length / 2))]
+
+   array.push(middle2, middle1)
+
+   return array;
+
+ }
+ }
 
 function increasing(numbers) {
   // write your code here
